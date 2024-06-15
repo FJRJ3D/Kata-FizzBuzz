007ac3bfe03ad36b40fe52f57e0f480471ad7774
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-15 23:42:47 +0200

    update

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -8,12 +8,12 @@ public class App {
             int number1 = (index/10)%10;
             int number2 = index%10;
 
-            if (index % 3 == 0 && index % 5 == 0 || index%3 ==0 && number1 ==5 ||index%3 ==0 &&  number2 ==5 || index%5 ==0 && number1==3||index%5 ==0 &&number2==3){
-                System.out.println(index+"FizzBuzz");
+            if (index % 3 == 0 && index % 5 == 0 || index%3 ==0 && number1 ==5 ||index%3 ==0 &&  number2 ==5 || index%5 ==0 && number1==3||index%5 ==0 &&number2==3||number1==3&&number2==5||number1==5&&number2==3){
+                System.out.println("FizzBuzz");
             }else if (number1 ==3 || index%3 == 0||number2==3) {
-                System.out.println(index+"Fizz");
+                System.out.println("Fizz");
             }else if (number1==5 || index%5 ==0||number2==5) {
-                System.out.println(index+"Buzz");
+                System.out.println("Buzz");
             }else {
                 System.out.println(index);
             }
