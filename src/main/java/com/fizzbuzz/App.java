a74844e8d344cb14dad1df71b2f7bca5e3422785
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-15 23:33:29 +0200

    update

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -3,15 +3,17 @@ package com.fizzbuzz;
 public class App {
 
     public static void main( String[] args ) {
-        for(int index =1; 100>=index; index++) {
-            if (index%3 == 0){
-                if (index%5 == 0){
-                    System.out.println("FizzBuzz");
-                }else {
-                    System.out.println("Fizz");
-                }
-            }else if (index%5 == 0){
-                System.out.println("Buzz");
+
+        for (int index =1; 100>=index; index++){
+            int number1 = (index/10)%10;
+            int number2 = index%10;
+
+            if (index % 3 == 0 && index % 5 == 0 || index%3 ==0 && number1 ==5 ||index%3 ==0 &&  number2 ==5 || index%5 ==0 && number1==3||index%5 ==0 &&number2==3){
+                System.out.println(index+"FizzBuzz");
+            }else if (number1 ==3 || index%3 == 0||number2==3) {
+                System.out.println(index+"Fizz");
+            }else if (number1==5 || index%5 ==0||number2==5) {
+                System.out.println(index+"Buzz");
             }else {
                 System.out.println(index);
             }
