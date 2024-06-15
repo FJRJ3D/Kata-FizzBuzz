1d999af536a54abe5626be324a968d9659fdeab7
Francisco José Retamero Jaime <fjrj3d@gmail.com>
2024-06-15 18:48:01 +0200

    update

--- src/main/java/com/fizzbuzz/App.java
+++ src/main/java/com/fizzbuzz/App.java
@@ -1,13 +1,20 @@
 package com.fizzbuzz;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) {
+        for(int index =1; 100>=index; index++) {
+            if (index%3 == 0){
+                if (index%5 == 0){
+                    System.out.println("FizzBuzz");
+                }else {
+                    System.out.println("Fizz");
+                }
+            }else if (index%5 == 0){
+                System.out.println("Buzz");
+            }else {
+                System.out.println(index);
+            }
+        }
     }
 }
